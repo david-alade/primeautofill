2d16f4fd72c8c9c6b1273db54856c3000f410152
David Alade <davidalade1051@gmail.com>
2020-07-22 23:58:10 -0400

    dad

--- discordauth/index.js
+++ discordauth/index.js
@@ -66,7 +66,8 @@ console.log("Our user's id is " + usersId);
 
 client.once('ready', () => {
   console.log('Ready!');  
-  const testDisc = client.guilds.cache.get('719970136995397732');
+  //const testDisc = client.guilds.cache
+  const members = message.guild.members.cache.get('719970136995397732');
   console.log(testDisc);
 });
 
