36bad6f43c37834c75b0832bb765b4f6470aef0c
David Alade <davidalade1051@gmail.com>
2020-07-23 02:10:34 -0400

    dad

--- discordauth/index.js
+++ discordauth/index.js
@@ -71,7 +71,7 @@ client.once('ready', () => {
   const testDisc = client.guilds.cache.get('719970136995397732').members.fetch()
     .then(user => {
       arrayOfIds.push(user.id);
-      console.log(arrayOfIds);
+      console.log(user);
     })
 });
 
